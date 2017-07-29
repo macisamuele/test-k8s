b9370b9e29a3891f1190efeda5778912d4cd7c25
Samuele Maci <maci@yelp.com>
2017-07-29 20:51:33 +0200

    Configure volumes Simplify install

--- k8s/kubernetes fresh install.sh
+++ k8s/kubernetes fresh install.sh
@@ -1,25 +1,49 @@
-# Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#!/bin/bash
+set -eu
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Install kubectl
-brew install kubectl
+cd ${DIR}
 
-# Install Virtualbox
-wget http://download.virtualbox.org/virtualbox/5.1.24/VirtualBox-5.1.24-117012-OSX.dmg
-# Manual install it!
+# Check presence of virtualbox
+if [ ! ls /Application/Virtualbox.app ]
+then
+    echo "Virtualbox is not installed!"
+    echo "Please run download and install it from https://www.virtualbox.org/wiki/Downloads"
+    exit 1
+fi
 
-# Download minukube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.21.0/minikube-darwin-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+echo "Verify that homebrew is installed"
+if [ ! hash brew ]
+then
+    echo "homebrew is not installed ... installing it!"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
+echo "Verify that kubectl is installed"
+if [ ! hash kubectl ]
+then
+    echo "kubectl is not installed ... installing it!"
+    brew install kubectl
+fi
 
-# Start minikube
+echo "Verify that minikube is installed"
+if [ ! hash minikube ]
+then
+    echo "minikube is not installed ... installing it!"
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+fi
+
+echo "Starting minikube"
 minikube start --memory 4096 --cpus 4 --disk-size 40g --kubernetes-version v1.7.0
 
-# Open dashboard
-minikube dashboard
+echo "Updating ssh fingerprints (~/.ssh/known_hosts)"
+ssh-keygen -R $(minikube ip)
+ssh-keyscan -H $(minikube ip) >> ~/.ssh/known_hosts
 
-# Copy a directory into minikube VM
+echo "Copying $(dirname $(pwd)) in /home/docker of minikube"
 scp -rp -i ~/.minikube/machines/minikube/id_rsa $(dirname $(pwd))/* docker@$(minikube ip):/home/docker/
 
-# Build dockerfiles
+echo "Building base docker images inside minikube"
 minikube ssh ./dockerfiles/build.sh
+
+cd -
